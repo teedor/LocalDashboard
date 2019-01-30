f829f77d8fb76645c4ff2181c9a9ce64cd3f7c7b
Stephen Anderson <stephen.anderson@madetoengage.com>
2019-01-30 14:10:29 +0000

    Add project files.

---
+++ LocalDashboard.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocalDashboard.Web.MvcApplication" Language="C#" %>
---
+++ LocalDashboard.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocalDashboard.Web.Startup))]
+namespace LocalDashboard.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
