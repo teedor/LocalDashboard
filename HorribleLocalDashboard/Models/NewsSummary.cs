514a252505260cf4054e8b822dbeb383f2f49c75
Stephen Anderson <stephen.anderson@madetoengage.com>
2019-02-07 17:01:03 +0000

    horrible dashboard works

---
+++ HorribleLocalDashboard/Models/NewsSummary.cs
@@ -0,0 +1,11 @@
+﻿namespace HorribleLocalDashboard.Models
+{
+    public class NewsSummary
+    {
+        public string Source { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PublishedDateLocalTime { get; set; }
+    }
+}
---
+++ HorribleLocalDashboard/Models/TimeZoneDbApiResponseModel.cs
@@ -0,0 +1,19 @@
+﻿namespace HorribleLocalDashboard.Models
+{
+    public class TimeZoneDbApiResponseModel
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+        public string countryCode { get; set; }
+        public string countryName { get; set; }
+        public string zoneName { get; set; }
+        public string abbreviation { get; set; }
+        public int gmtOffset { get; set; }
+        public string dst { get; set; }
+        public int zoneStart { get; set; }
+        public int zoneEnd { get; set; }
+        public string nextAbbreviation { get; set; }
+        public int timestamp { get; set; }
+        public string formatted { get; set; }
+    }
+}
