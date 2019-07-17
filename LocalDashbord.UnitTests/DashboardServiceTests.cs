8afa97cdf2beacdbcd3988cbfe5eafa713a41ba9
Stephen Anderson <stephen.anderson@madetoengage.com>
2019-07-17 15:28:03 +0100

    renamed bad spelling project and fixed non-nullable ints in time connectodotr

--- LocalDashbord.UnitTests/DashboardServiceTests.cs
+++ LocalDashbord.UnitTests/DashboardServiceTests.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LocalDashbord.UnitTests
+namespace LocalDashboard.UnitTests
 {
     [TestFixture]
     public class DashboardServiceTests
